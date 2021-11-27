67d9c330f9963e0dc081da131fea61271207ce4a
Илья Филиппов <filippov.i.s@gmail.com>
2021-11-27 15:24:19 +0300

    Добавьте файлы проекта.

---
+++ Program_4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите целое число больше 0 = ");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int a = 0;
+
+            for (int i = 2 * N - 1; i >0; i-=2)
+            {
+                a += i;                
+            }
+            Console.WriteLine("Квадрат числа {0} равен {1}", N, a);
+
+            Console.WriteLine("Нажмите на любого клавиша");
+            Console.ReadKey();
+        }
+    }
+}
